93673c3900c3feeded47b77f915f2481b7f7fe20
Phil Curran <pecurran@hotmail.com>
2021-12-18 20:07:49 -0800

    got size radio buttons working

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 const basePrice = 10;
-
-const sizes = {
-  small: 1,
-  medium: 1.5,
-  large: 2,
-};
+const taxRate = 1.1;
 
 const meats = {
   pepperoni: 1.5,
@@ -28,25 +23,41 @@ const veggies = {
   garlic: 1,
   basil: 1,
 };
+
+let getSize = document.getElementsByClassName("size");
+
+let sizeMultiplier = () => {
+  if (getSize[0].checked) {
+    return 1;
+  } else if (getSize[1].checked) {
+    return 1.5;
+  } else if (getSize[2].checked) {
+    return 2;
+  } else {
+    console.log("no size selected");
+  }
+};
+
 function Pizza(size) {
   this.size = size;
+  this.basePrice = size * basePrice;
   this.meats = [];
   this.cheeses = [];
   this.veggies = [];
 }
 
-Pizza.prototype.addMeat = function (meat) {
-  // this.meats = [];
-  this.meats.push(meat);
-};
+// Pizza.prototype.addMeat = function (meat) {
+//   // this.meats = [];
+//   this.meats.push(meat);
+// };
 
-Pizza.prototype.addCheese = function (cheese) {
-  // this.cheese = [];
-  this.cheeses.push(cheese);
-};
+// Pizza.prototype.addCheese = function (cheese) {
+//   // this.cheese = [];
+//   this.cheeses.push(cheese);
+// };
 
-Pizza.prototype.addVeggies = function (veggies) {
-  let temp = Array.from(veggies.split(" "));
-  console.log(temp);
-  this.veggies.push(temp);
-};
+// Pizza.prototype.addVeggies = function (veggies) {
+//   let temp = Array.from(veggies.split(" "));
+//   console.log(temp);
+//   this.veggies.push(temp);
+// };
