d3dab4166e861bb5836d37a7b691ec094f41ea60
Phil Curran <pecurran@hotmail.com>
2021-12-19 22:24:43 -0800

    cleaned up main object and made prototypes for pizza changes.

--- js/script.js
+++ js/script.js
@@ -1,73 +1,79 @@
-const basePrice = 10;
-const taxRate = 1.1;
+// Biz Logic
 
+// get size from form
 let getSize = document.getElementsByClassName("size");
+// get meat selections from form
+let selectedMeats = Array.from(document.getElementsByClassName("meat"));
+// get cheese selections from form
+let selectedCheeses = Array.from(document.getElementsByClassName("cheese"));
+// get veggies selections from form
+let selectedVeggies = Array.from(document.getElementsByClassName("veggie"));
 
-let sizeMultiplier = () => {
-  if (getSize[0].checked) {
-    return 1;
-  } else if (getSize[1].checked) {
-    return 1.5;
-  } else if (getSize[2].checked) {
-    return 2;
-  } else {
-    console.log("no size selected");
-  }
-};
-
-let meat = Array.from(document.getElementsByClassName("meat"));
-
-let meatCount = 0;
-
-let a = () => {
-  meat.forEach((option) => {
-    if (option.checked) {
-      meatCount += 1.5;
-    }
-  });
-};
-
-let cheese = Array.from(document.getElementsByClassName("cheese"));
-
-let cheeseCount = 0;
-
-let b = () => {
-  cheese.forEach((option) => {
-    if (option.checked) {
-      cheeseCount += 1;
-    }
-  });
-};
-
-var form = document.querySelector("form");
-form.addEventListener("change", function () {
-  console.log("form change");
-  a();
-  console.log(meatCount);
-  b();
-  console.log(cheeseCount);
-});
-
-function Pizza(size) {
-  this.size = size;
-  this.basePrice = size * basePrice;
+// main Pizza object
+function Pizza() {
+  this.size = undefined;
   this.meats = [];
   this.cheeses = [];
   this.veggies = [];
 }
 
-// Pizza.prototype.addMeat = function (meat) {
-//   // this.meats = [];
-//   this.meats.push(meat);
-// };
 
-// Pizza.prototype.addCheese = function (cheese) {
-//   // this.cheese = [];
-//   this.cheeses.push(cheese);
-// };
+  
+  Pizza.prototype.updateSize() {
+    if (getSize[0].checked) {
+      return 1;
+    } else if (getSize[1].checked) {
+      return 1.5;
+    } else if (getSize[2].checked) {
+      return 2;
+    } else {
+      console.log("no size selected");
+    }
+  }
+  
+  Pizza.prototype.addMeats() {
+    meat.forEach((option) => {
+      if (option.checked) {
+        meatCount += 1.5;
+      }
+    });
+  };
+  
+  Pizza.prototype.addCheeses() {
+    cheese.forEach((option) => {
+      if (option.checked) {
+        cheeseCount += 1;
+      }
+    });
+  };
+  
+  Pizza.prototype.addVeggies() {
+    cheese.forEach((option) => {
+      if (option.checked) {
+        cheeseCount += 1;
+      }
+    });
+  };
+  
+  Pizza.prototype.getSubtotal() {
+  
+  };
+  
+  let pizza = new Pizza();
 
-// Pizza.prototype.addVeggies = function (veggies) {
-//   let temp = Array.from(veggies.split(" "));
-//   console.log(temp);
-//   this.veggies.push(temp);
-// };
+// UI Logic
+
+// get form as a variable
+var form = document.querySelector("form");
+
+// set event listener for form to track changes
+form.addEventListener("change", function () {
+  console.log("form change");
+
+  console.log(pizza.meats);
+
+  console.log(pizza.cheeses);
+});
+
+const basePrice = 10;
+const taxRate = 1.1;
